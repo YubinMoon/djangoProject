409d0f938f35b672b0eb91ac3c839b2dbe7eb1bd
terry <munu220@gmail.com>
2023-06-26 14:35:45 +0900

    add blog list and detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 app_name = "blog"
 urlpatterns = [
     path("", views.PostLV.as_view(), name="index"),
+    re_path(r"^post/(?P<slug>[-\w]+)/$", views.PostDV.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
@@ -9,3 +9,7 @@ class PostLV(ListView):
     template_name = "blog/post_all.html"
     context_object_name = "posts"
     paginate_by = 2
+
+
+class PostDV(DetailView):
+    model = Post

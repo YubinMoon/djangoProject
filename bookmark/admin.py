d78db3500e190c98a0cf0a560a4ed3b52d9f1078
terry <munu220@gmail.com>
2023-06-26 11:06:46 +0900

    add Bookmark admin

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bookmark.models import Bookmark
 
-# Register your models here.
+# 데코레이터 패턴을 사용하지 않는 방법
+# admin.site.register(Bookmark)
+
+
+@admin.register(Bookmark)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "url")

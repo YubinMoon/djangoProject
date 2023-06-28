916bdbb38ca33332180012c4dcbabdcbb1acc1bb
terry <munu220@gmail.com>
2023-06-28 10:11:02 +0900

    disqus 완료

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,3 +136,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+DISQUS_SHORTNAME = "test-gipofq2rw4"
+DISQUS_MY_DOMAIN = "http://127.0.0.1:8000/"
